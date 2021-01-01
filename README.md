bdbc4eae68ade4e2b44938b283e55e4745234609
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-01 14:54:13 +0000

    changed my formats

--- README.md
+++
@@ -1,2 +0,0 @@
-# ICS2O.R.-Unit-6-01
- [![linter](https://github.com/Huzaifa-Khalid2/ICS2O.R.-Unit-6-01/workflows/linter/badge.svg)](https://github.com/marketplace/actions/super-linter) 
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.getElementById('my-btn').addEventListener('click', myFunction)
 let number = 0
-function myFunction () {
+function myFunction() {
   number = document.getElementById('my-text').value
-  number = parseInt(number)
-  if (number === '3') {
-    alert('true')
-}
-  else { 
+  number = parseInt (number)
+  if (number == '3') {
+    alert ('true')
+  }
+  else 
+  { 
     alert('false')
   }
 }
